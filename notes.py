57975605942cc820bbf67fddb2501d9a29fdc345
liambarry7 <74929060+liambarry7@users.noreply.github.com>
2026-04-07 14:56:48 +0100

    notes

---
+++ notes.py
@@ -0,0 +1,22 @@
+"""
+    Tasks:
+        - Prompt Engineering vs ML
+
+
+    ML models:
+        - Decision Tree (scikit) - drop if random forest is better
+        - Random Forest (scikit)
+        - kNN (scikit)
+        - Gradient Boosting (XGBoost)
+        - Simple Neural Networks (scikit/Tensorflow) - MLP
+        - Deep Learning Neural Networks (Tensorflow)
+
+        Random Forest
+        XGB
+        ANN
+        SVM
+
+    Prompt Engineering:
+        - Gemma-2b
+
+"""
