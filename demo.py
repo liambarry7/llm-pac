97378ec2fa914bedded3e4193eba0287444ce98b
liambarry7 <74929060+liambarry7@users.noreply.github.com>
2026-05-22 11:49:18 +0100

    ran RF+CoT successfully

--- demo.py
+++ demo.py
@@ -3,7 +3,20 @@
 
     Things to demo:
     - LLM pipelines (test on little data)
-    - Demo best ML models - use pickle models
+    - Demo best ML models - use pickle models (MLP)
 
 """
+from feature_preprocessing import get_feature_dataset, get_llm_dataset
 
+def demo_mlp():
+    pass
+
+if __name__ == "__main__":
+    # best ml = mlp
+
+    model_type = input("Enter model type ('llm' or 'ml')")
+    if model_type == "llm":
+        x_train, x_test, y_train, y_test = get_llm_dataset()
+
+    elif model_type == "ml":
+        x_train, x_test, y_train, y_test = get_feature_dataset()
